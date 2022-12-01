23c8f9737722c470394a10aac4a7c671008b0d1b
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-01 08:38:29 +0530

    Add files via upload

---
+++ amazonclone/src/Components/Signinsidebar/Signinsidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import './Signinsidebar.css';
+export default function (){
+  return (
+    <div>
+      <AccountCircleRoundedIcon/>
+      <button className = "button">Hello, Sign in</button>
+    </div>
+
+  );
+}
