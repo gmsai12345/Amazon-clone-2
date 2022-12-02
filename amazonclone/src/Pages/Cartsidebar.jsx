db56d28c9820a0ce21b9ae790b59b6e703a7a030
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-02 17:47:02 +0530

    Create Cartsidebar.jsx

---
+++ amazonclone/src/Pages/Cartsidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StarIcon from '@mui/icons-material/Star';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+
+export default function () {
+  return (
+    <div>
+      <span>Your recently viewed items</span>
+      <img src = "" alt = ""/><span>Drools Adult(+1 year)
+      </span>
+      <span><StarIcon/><StarIcon/><StarIcon/><StarIcon/><StarOutlineIcon/></span>
+      <img src = "" alt = ""/>
+      <span>Drools Adult(+1 year)
+      </span>
+      <span><StarIcon/><StarIcon/><StarIcon/><StarIcon/><StarHalfIcon/></span>
+
+      <img src = "" alt = ""/>
+      <span>Drools Adult(+1 year)
+      </span>
+      <span><StarIcon/><StarIcon/><StarIcon/><StarIcon/><StarOutlineIcon/></span>
+      <img src = "" alt = ""/>
+      <span>Drools Adult(+1 year)
+      </span>
+      <span><StarIcon/><StarIcon/><StarIcon/><StarIcon/><StarHalfIcon/></span>
+    </div>
+  );
+}
